1ab62fc64799d18fa8904a1b48210253a6bbe145
Mark Janes <mark.a.janes@intel.com>
2014-08-19 11:18:14 -0700

    add build_support init
    For now, most of build_support is commented out.  As modules are
    ported, they will be re-enabled in __init__.py

---
+++ build_support/__init__.py
@@ -0,0 +1,112 @@
+#!/usr/bin/python
+
+import time
+from command import rmtree
+from command import rmfile
+#from command import killMajorProcesses
+from options import Options
+#from component_map import ComponentMap
+#from export import Export
+#from export import ExportTest
+#from export import ExportSymbols
+#from jenkins import Jenkins
+#from spec import BuildInvoke
+#from spec import BuildSpec
+#from export import ExportZip
+#from gtest import GTest as GTest
+#from clean_server import CleanServer
+#from timer import TimeOut
+from repo_status import RepoStatus
+
+class DefaultTimeout:
+    def __init__(self, options=None):
+        self._options = options
+        if not options:
+            self._options = Options()
+
+    def GetDuration(self):
+        """by default, components should finish in under 15 minutes.
+        For daily builds, 60 minutes is acceptable."""
+
+        if self._options.type == "daily" or self._options.type == "release":
+            return 60
+        return 15
+
+def null_build():
+    pass
+
+class NullInvoke:
+    """masquerades as an invoke object, so the main routine can post
+    results even if there is no server to post to"""
+    def __init__(self):
+        pass
+    
+    def set_info(self, *args):
+        pass
+
+    def set_status(self, *args):
+        pass
+
+def build(builder, options=None, time_limit=None):
+    if not time_limit:
+        time_limit = DefaultTimeout()
+    if not options:
+        options = Options()
+    action_map = {
+               "build": builder.build,
+               "clean": builder.clean,
+               "test" : builder.test,
+              }
+    actions = options.action
+
+    invoke = NullInvoke()
+    if (options.result_path):
+        # if we aren't posting to a server, don't attempt to write
+        # status
+        invoke = BuildInvoke(options)
+
+    invoke.set_info("start_time", time.time())
+
+    # start a thread to limit the run-time of the build
+    to = TimeOut(time_limit)
+    to.start()
+
+
+    if type(actions) is str:
+        actions = [actions]
+
+    if "test" in actions:
+        killMajorProcesses()
+
+    for a in actions:
+        options.action = a
+        if a == "test":
+            # Jenkins requires at least one result file to pass a
+            # build, even if the gasket does not run any tests.
+            test_dir = os.path.join(ComponentMap(options).bin_dir(), "test")
+            if not os.path.exists(test_dir):
+                os.makedirs(test_dir)
+            test_file = os.path.join(test_dir, "gtest_bogus_{0}.xml".format(options.hardware))
+            f = open(test_file, "w")
+            f.write( GTest.bogus_gtest_data )
+            f.close()
+
+        if options.action in action_map:
+            try:
+                action_map[options.action]()
+            except:
+                # we need to cancel the timer first, in case
+                # set_status fails, and the timer is left running
+                to.end()
+                invoke.set_status("failed")
+                # must cancel timeout timer, which will prevent process from ending
+                raise        
+                
+    # must cancel timeout timer, which will prevent process from
+    # ending.  cancel the timer first, in case set_status fails, and
+    # the timer is left running
+    to.end()
+    invoke.set_info("end_time", time.time())
+    invoke.set_status("success")
+
+
