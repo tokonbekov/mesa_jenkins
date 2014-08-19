479e3d70b2a86e10f86245029c5aa265f239abe1
Mark Janes <mark.a.janes@intel.com>
2014-08-19 15:47:05 -0700

    refactor "default" tag from build specification to be a "repos" tag.
    Repositories are fixed, at most one per project, and they include all
    remote references.

--- build_support/repo_status.py
+++ build_support/repo_status.py
@@ -4,15 +4,11 @@ import xml.etree.ElementTree as ET
 from . import ProjectMap
 
 class _ProjectBranch:
-    def __init__(self, tag):
-        self.name = str(tag.tag)
-        self.repo_url = None
-        self.branch = None
+    def __init__(self, projectName):
+        # default to master branch
+        self.branch = "origin/master"
+        self.name = projectName
         self.sha = None
-        if tag.attrib.has_key("repo"):
-            self.repo_url = tag.attrib["repo"]
-        if tag.attrib.has_key("branch"):
-            self.branch = tag.attrib["branch"]
 
 class _BranchSpecification:
     """This class tracks a "branch set" in the build's git repositories
@@ -20,30 +16,29 @@ class _BranchSpecification:
     branches will result in a world build.
 
     """
-    def __init__(self, spec_tag, default_tag, repos):
+    def __init__(self, branch_tag, repos):
         self._project_branches = {}
-        self.name = spec_tag.attrib["name"]
+        self.name = branch_tag.attrib["name"]
+        self.project = branch_tag.attrib["project"]
 
-        # start with the defaults
-        for a_project in default_tag:
-            pb = _ProjectBranch(a_project)
-            self._project_branches[pb.name] = pb
+        # by default, all repos are at origin/master
+        for (name, _) in repos.iteritems():
+            pb = _ProjectBranch(name)
+            self._project_branches[name] = pb
 
         # override the defaults
-        for a_project in spec_tag:
-            pb = _ProjectBranch(a_project)
-            if (pb.repo_url):
-                self._project_branches[pb.name].repo_url = pb.repo_url
-            if (pb.branch):
-                self._project_branches[pb.name].branch = pb.branch
+        for a_project in branch_tag:
+            name = a_project.tag
+            assert(self._project_branches.has_key(name))
+            self._project_branches[name].branch = a_project.attrib["branch"]
 
         self.update_commits(repos)
 
     def update_commits(self, repos):
         # get the current commit for each project
         for (_, branch) in self._project_branches.iteritems():
-            assert (repos.has_key(branch.repo_url))
-            repo = repos[branch.repo_url]
+            assert (repos.has_key(branch.name))
+            repo = repos[branch.name]
             for remote in repo.remotes:
                 remote.fetch()
             branch.sha = str(repo.commit(branch.branch))
@@ -52,7 +47,7 @@ class _BranchSpecification:
         # checks the commits on the branch repos to see if they have
         # been updated.
         for (_, branch) in self._project_branches.iteritems():
-            repo = repos[branch.repo_url]
+            repo = repos[branch.name]
             for remote in repo.remotes:
                 remote.fetch()
             if branch.sha != str(repo.commit(branch.branch)):
@@ -65,22 +60,23 @@ class RepoStatus:
         # all repositories are cloned into this dir, so they can be cleaned up
         self._repo_dir = ProjectMap().source_root() + "/repos"
         
-        # key is url, value is repo object
+        # key is project, value is repo object
         self._repos = {}
 
         self._branches = []
 
         buildspec = ET.parse(buildspec)
         branches = buildspec.find("branches")
-        default = branches.find("default")
+        repos = buildspec.find("repos")
 
         # fetch all the repos into _repo_dir
-        for tag in default:
+        for tag in repos:
             url = tag.attrib["repo"]
-            if (self._repos.has_key(url)):
+            project = tag.tag
+            if (self._repos.has_key(project)):
                 continue
 
-            project_repo_dir = self._repo_dir + "/" + tag.tag
+            project_repo_dir = self._repo_dir + "/" + project
             repo = None
             if not os.path.exists(project_repo_dir):
                 os.makedirs(project_repo_dir)
@@ -89,17 +85,10 @@ class RepoStatus:
                 repo = git.Repo(project_repo_dir)
                 for remote in repo.remotes:
                     remote.fetch()
-            self._repos[url] = repo
+            self._repos[project] = repo
 
-        for branch in branches:
-            for tag in branch:
-                if not tag.attrib.has_key("repo"):
-                    continue
-                url = tag.attrib["repo"]
-                assert (self._repos.has_key(url))
-        
         for branch in branches.findall("branch"):
-            self._branches.append(_BranchSpecification(branch, default, self._repos))
+            self._branches.append(_BranchSpecification(branch, self._repos))
 
 
     def poll(self):
