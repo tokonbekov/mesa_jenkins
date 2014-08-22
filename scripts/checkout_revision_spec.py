58242a914a43cab5d81704d6170e3c9e428333c5
Mark Janes <mark.a.janes@intel.com>
2014-08-22 14:37:05 -0700

    fix typo
    I accidentally checked in without flymake.

--- scripts/checkout_revision_spec.py
+++ scripts/checkout_revision_spec.py
@@ -4,7 +4,7 @@ import sys, os
 sys.path.append(os.path.join(os.path.dirname(os.path.abspath(sys.argv[0])), ".."))
 import build_support as bs
 
-rs = bs.RevisionSpecification(argv[1])
+rs = bs.RevisionSpecification(sys.argv[1])
 
 rs.checkout()
 
