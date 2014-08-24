4c2e0925d6e2533d74b259e7c1d689e354e788e2
Mark Janes <mark.a.janes@intel.com>
2014-08-24 12:49:38 -0700

    add poll_branches.py
    this script runs forever, checking heads of all branches and
    triggering a jenkins build.

---
+++ scripts/poll_branches.py
@@ -0,0 +1,18 @@
+import sys, os, urllib2, time
+sys.path.append(os.path.join(os.path.dirname(os.path.abspath(sys.argv[0])), ".."))
+import build_support as bs
+
+status = bs.RepoStatus()
+
+pm = bs.ProjectMap()
+spec = pm.build_spec()
+server = spec.find("build_master").attrib["host"]
+while True:
+    branches = status.poll()
+    for branch in branches:
+        print "Building " + branch
+        job_url = "http://" + server + "/job/" + branch + "/build"
+        f = urllib2.urlopen(job_url)
+        f.read()
+
+    time.sleep(60)
