a41c47e0ae30d3acebff80f85cd35e4633bbae42
Mark Janes <mark.a.janes@intel.com>
2014-08-24 13:35:45 -0700

    Add cmake builder
    This commit adds buildscripts for waffle and piglit

--- build_support/builders.py
+++ build_support/builders.py
@@ -1,4 +1,4 @@
-import os, multiprocessing
+import os, multiprocessing, re
 from . import Options
 from . import ProjectMap
 from . import run_batch_command
@@ -54,3 +54,51 @@ class AutoBuilder(object):
             run_batch_command(["make", "distclean"])
         os.chdir(savedir)
         rmtree(self._build_dir)
+
+
+class CMakeBuilder(object):
+    def __init__(self):
+        self._options = Options()
+        self._project_map = ProjectMap()
+        project = self._project_map.current_project()
+
+        self._src_dir = self._project_map.project_source_dir(project)
+        self._build_root = self._project_map.build_root()
+        self._build_dir = self._src_dir + "/build_" + self._options.arch
+        self._build_dir = self._src_dir + "/build_" + self._options.arch
+
+    def build(self):
+        if not os.path.exists(self._build_dir):
+            os.makedirs(self._build_dir)
+
+        savedir = os.getcwd()
+        os.chdir(self._build_dir)
+
+        run_batch_command(["cmake", self._src_dir])
+
+        run_batch_command(["cmake", self._src_dir, "--build", ".", 
+                           "--", "-j" + str(multiprocessing.cpu_count() + 1)])
+        os.chdir(savedir)
+
+    def clean(self):
+        rmtree(self._build_dir)
+
+    def test(self):
+        savedir = os.getcwd()
+        os.chdir(self._build_dir)
+
+        # get test names
+        command = ["ctest", "-V", "-N"]
+        (out, err) = run_batch_command(command, streamedOutput=False, quiet=True)
+
+        os.chdir(savedir)
+
+        out = out.splitlines()
+        for aline in out:
+            # execute each command reported by ctest
+            match = re.match(".*Test command: (.*)", aline)
+            if not match:
+                continue
+            (bin_dir, exe) = os.path.split(match.group(1))
+            #bs.GTest(bin_dir, exe, working_dir=bin_dir).run_tests()
+    
---
+++ waffle/build.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python
+
+
+import sys, os
+sys.path.append(os.path.join(os.path.dirname(os.path.abspath(sys.argv[0])), ".."))
+import build_support as bs
+
+bs.build(bs.CMakeBuilder())
