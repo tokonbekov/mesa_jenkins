7d4eeb396b338228d76f26ccbd0c1aacca464ce4
Dylan Baker <baker.dylan.c@gmail.com>
2014-09-05 15:29:47 -0700

    piglit-build/build.py: Explicitly set build paramters
    This explicitly turns on GL, GLES1, GLES2, GLES3, and GLX tests, and
    turns of CL tests.
    
    This is a change from the default which does not build GLES tests

--- piglit-build/build.py
+++ piglit-build/build.py
@@ -5,4 +5,16 @@ import sys, os
 sys.path.append(os.path.join(os.path.dirname(os.path.abspath(sys.argv[0])), ".."))
 import build_support as bs
 
-bs.build(bs.CMakeBuilder())
+opts = [
+    '-DPIGLIT_BUILD_DMA_BUG_TESTS=1',
+    '-DPIGLIT_BUILD_GLES1_TESTS=1',
+    '-DPIGLIT_BUILD_GLES2_TESTS=1',
+    '-DPIGLIT_BUILD_GLES3_TESTS=1',
+    '-DPIGLIT_BUILD_GL_TESTS=1',
+    '-DPIGLIT_BUILD_GLX_TESTS=1',
+    '-DPIGLIT_BUILD_CL_TESTS=0',
+]
+
+builder = bs.CMakeBuilder(extra_definitions=opts)
+
+bs.build(builder)
