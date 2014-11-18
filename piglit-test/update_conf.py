10a7545d13a97adf043ac0119e42ab9fe74b0705
Mark Janes <mark.a.janes@intel.com>
2014-11-18 09:54:45 -0800

    add script to update piglit-test conf files
    This was done manually in the past, which was tedious.  This script
    automates the process.
    
    TODO: remove expected failures which are now passing.

---
+++ piglit-test/update_conf.py
@@ -0,0 +1,51 @@
+#!/usr/bin/python
+
+import xml.etree.ElementTree as ET
+import ConfigParser as CP
+import sys, os, glob
+
+script_dir = os.path.dirname(sys.argv[0])
+if not script_dir:
+    script_dir = "."
+script_dir = script_dir + "/"
+    
+xmls = []
+for arg in sys.argv[1:]:
+    xmls = xmls + glob.glob(arg)
+
+# needed to preserve case in the options
+class CaseConfig(CP.SafeConfigParser):
+    def optionxform(self, optionstr):
+        return optionstr
+
+for f in xmls:
+    print "parsing " + f
+    t = ET.parse(f)
+    r = t.getroot()
+
+    fn = os.path.splitext(os.path.split(f)[-1])[0]
+    hw = fn.split("_")[1]
+    if "gt" in hw:
+        hw = hw[:3]
+
+    arch = fn.split("_")[2]
+    conf_file = script_dir + hw + arch + ".conf"
+    if not os.path.exists(conf_file):
+        conf_file = script_dir + hw + ".conf"
+        assert(os.path.exists(conf_file))
+    print "updating " + conf_file
+    c = CaseConfig(allow_no_value=True)
+    c.optionxform = str
+    c.read(conf_file)
+    
+    for afail in r.findall(".//failure/.."):
+        # strip the arch/hw off the end of the name
+        name = ".".join(afail.attrib["name"].split(".")[:-1])
+        c.set("expected-failures", afail.attrib["classname"] + "." + name)
+
+    for acrash in r.findall(".//error/.."):
+        # strip the arch/hw off the end of the name
+        name = ".".join(acrash.attrib["name"].split(".")[:-1])
+        c.set("expected-crashes", acrash.attrib["classname"] + "." + name)
+
+    c.write(open(conf_file, "w"))
