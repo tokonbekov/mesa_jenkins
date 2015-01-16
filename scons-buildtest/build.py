2cff73a147c4b951e0f9949a555f940ea2b478da
Mark Janes <mark.a.janes@intel.com>
2015-01-16 14:14:23 -0800

    add a builder to test the scons build

---
+++ scons-buildtest/build.py
@@ -0,0 +1,37 @@
+#!/usr/bin/python
+
+import sys
+import os
+import os.path as path
+import multiprocessing
+sys.path.append(path.join(path.dirname(path.abspath(sys.argv[0])), ".."))
+import build_support as bs
+
+class SconsBuilder(object):
+    def __init__(self):
+        self.src_dir = bs.ProjectMap().source_root() + "/repos/mesa"
+
+    def clean(self):
+        save_dir = os.getcwd()
+        os.chdir(self.src_dir)
+        bs.run_batch_command(["scons", "-c"])
+        bs.rmtree("build")
+        os.chdir(save_dir)
+
+    def test(self):
+        pass
+
+    def build(self):
+        save_dir = os.getcwd()
+        os.chdir(self.src_dir)
+        bs.run_batch_command(["scons", "-j",
+                              str(multiprocessing.cpu_count() + 1)])
+        os.chdir(save_dir)
+        
+def main():
+    b = SconsBuilder()
+    bs.build(b)
+
+
+if __name__ == '__main__':
+    main()
