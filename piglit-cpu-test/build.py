20bc143a65fa57d32774265a5c1a50341f366559
Mark Janes <mark.a.janes@intel.com>
2015-02-13 09:55:15 -0800

    fix-up start time data point for cpu-test.
    piglit-cpu-test misuses/reuses the build() routine, which results in
    overwriting the start time.

--- piglit-cpu-test/build.py
+++ piglit-cpu-test/build.py
@@ -1,8 +1,17 @@
 #!/usr/bin/python
 
-import sys, os, argparse
+import os
+import sys
+import time
 sys.path.append(os.path.join(os.path.dirname(os.path.abspath(sys.argv[0])), ".."))
 import build_support as bs
+
+# HACK
+# repeatedly invoking build() will overwrite the start times.  Record
+# it now and write the correct value when the last build() has
+# finished.  build() was not intended to be used this way.
+
+start_time = time.time()
 
 bs.build(bs.PiglitTester("", _suite="cpu", device_override="byt"))
 bs.build(bs.PiglitTester("", _suite="cpu", device_override="g45"))
@@ -14,3 +23,7 @@ bs.build(bs.PiglitTester("", _suite="cpu", device_override="hswgt3"))
 bs.build(bs.PiglitTester("", _suite="cpu", device_override="bdwgt2"))
 bs.build(bs.PiglitTester("", _suite="cpu", device_override="chv"))
 bs.build(bs.PiglitTester("", _suite="cpu", device_override="sklgt3"))
+
+options = bs.Options()
+if (options.result_path):
+    bs.ProjectInvoke(options).set_info("start_time", start_time)
