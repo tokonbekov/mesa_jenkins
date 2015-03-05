2ed567b29d8d31b234f31d38ab54bd6c015fa3bf
Mark Janes <majanes@lucy.jf.intel.com>
2015-03-05 08:45:13 -0800

    fix piglit bisector and piglit fails

--- piglit-test/add_piglit_failures.py
+++ piglit-test/add_piglit_failures.py
@@ -6,17 +6,34 @@ sys.path.append(current_dir + "/..")
 import build_support as bs
 
 parser = argparse.ArgumentParser(description="bisects all piglit failures")
-parser.add_argument("--bad_rev", type=str)
+parser.add_argument("--test_dir", type=str)
 parser.add_argument("--good_rev", type=str)
 args = parser.parse_args(sys.argv[1:])
 
+# get revisions from out directory
+dirnames = os.path.abspath(args.test_dir).split("/")
+if dirnames[-1] != "test":
+    args.test_dir = args.test_dir + "/test"
+    dirnames = os.path.abspath(args.test_dir).split("/")
+hash_dir = dirnames[5]
+revs = hash_dir.split("_")
+
+spec_xml = bs.ProjectMap().build_spec()
+results_dir = spec_xml.find("build_master").attrib["results_dir"]
+
 repos = bs.RepoSet()
-_revspec = bs.RevisionSpecification(from_cmd_line=args.bad_rev.split())
+_revspec = bs.RevisionSpecification(from_cmd_line=revs)
 _revspec.checkout()
 
-hash_str = _revspec.to_cmd_line_param().replace(" ", "_")
-new_failures = bs.TestLister("/mnt/jenkins/results/mesa_master/" +
-                             hash_str + "/daily/test/")
+def make_test_list(testlister):
+    _test_list = []
+    for atest in testlister.Tests():
+        test_name_good_chars = re.sub('[_ !:=]', ".", atest.test_name)
+        _test_list.append(test_name_good_chars + ".all_platforms")
+    return "--piglit_test=" + ",".join(_test_list)
+
+new_failures = bs.TestLister(args.test_dir)
+test_arg = make_test_list(new_failures)
 
 good_revisions = {}
 for arev in args.good_rev.split():
@@ -33,35 +50,51 @@ for commit in piglit_repo.iter_commits(max_count=1000):
     if good_revisions["piglit-build"] in commit.hexsha:
         break
 
+# retest build, in case expected failures has been updated
+# copy build root to bisect directory
+bisect_dir = results_dir + "/bisect/" + hash_dir
+cmd = ["rsync", "-rlptD", "/".join(dirnames[:-1]) +"/", bisect_dir]
+bs.run_batch_command(cmd)
+bs.rmtree(bisect_dir + "/test")
+bs.rmtree(bisect_dir + "/piglit-test")
+
+j=bs.Jenkins(_revspec, bisect_dir)
+o = bs.Options(["bisect_all.py"])
+o.result_path = bisect_dir
+depGraph = bs.DependencyGraph(["piglit-gpu-all"], o)
+print "Retesting piglit to: " + bisect_dir
+j.build_all(depGraph, extra_arg=test_arg, print_summary=False)
+new_failures = bs.TestLister(bisect_dir + "/test/")
+
+if not new_failures.Tests():
+    print "All tests fixed"
+    sys.exit(0)
+
+test_arg = make_test_list(new_failures)
+
+# build old piglit to see what piglit regressions were
 revspec = bs.RevisionSpecification(from_cmd_line=["piglit-build=" + piglit_commits[-1].hexsha])
 revspec.checkout()
 revspec = bs.RevisionSpecification()
 hashstr = revspec.to_cmd_line_param().replace(" ", "_")
-spec_xml = bs.ProjectMap().build_spec()
-results_dir = spec_xml.find("build_master").attrib["results_dir"]
-out_dir = "/".join([results_dir, "bisect", hashstr])
+old_out_dir = "/".join([results_dir, "bisect", hashstr])
+bs.rmtree(old_out_dir + "/test")
+bs.rmtree(old_out_dir + "/piglit-test")
 
-j=bs.Jenkins(revspec, out_dir)
+j=bs.Jenkins(revspec, old_out_dir)
 o = bs.Options(["bisect_all.py"])
-o.result_path = out_dir
+o.result_path = old_out_dir
 depGraph = bs.DependencyGraph(["piglit-gpu-all"], o)
-print "Building old piglit to: " + out_dir
-
-test_list = []
-for atest in new_failures.Tests():
-    test_name_good_chars = re.sub('[_ !:=]', ".", atest.test_name)
-    test_list.append(test_name_good_chars + ".all_platforms")
-test_arg = "--piglit_test=" + ",".join(test_list)
-
+print "Building old piglit to: " + old_out_dir
 
 j.build_all(depGraph, extra_arg=test_arg, print_summary=False)
-tl = bs.TestLister(out_dir + "/test/")
+tl = bs.TestLister(old_out_dir + "/test/")
 print "failures due to piglit:"
 piglit_failures = new_failures.TestsNotIn(tl)
 for a_test in piglit_failures:
     a_test.Print()
 
 for a_test in piglit_failures:
-    a_test.Bisect("piglit-build", piglit_commits)
+    a_test.Bisect("piglit-test", piglit_commits)
     a_test.UpdateConf(current_dir)
 
