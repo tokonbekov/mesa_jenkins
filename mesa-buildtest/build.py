32e292cc9d0f857dc71c915e25646a194feb66c6
Mark Janes <mark.a.janes@intel.com>
2015-03-06 14:37:47 -0800

    change buildtest to just print a bad junit test when it fails, instead of stopping the build

--- mesa-buildtest/build.py
+++ mesa-buildtest/build.py
@@ -1,8 +1,10 @@
 #!/usr/bin/python
 
 
-import sys
+import os
 import os.path as path
+import subprocess
+import sys
 sys.path.append(path.join(path.dirname(path.abspath(sys.argv[0])), ".."))
 import build_support as bs
 
@@ -28,8 +30,24 @@ def main():
 
     builder = bs.AutoBuilder(configure_options=options, export=False)
 
-    bs.build(builder)
-
+    try:
+        bs.build(builder)
+    except subprocess.CalledProcessError as e:
+        test_path = path.abspath(bs.ProjectMap().build_root() + "/../test/")
+        if not path.exists(test_path):
+            os.makedirs(test_path)
+        # filname has to begin with piglit for junit pattern match in jenkins to find it.
+        fh = open(test_path + "/piglitmesa-buildtest_" + global_opts.arch + ".xml", "w")
+        fh.write("""\
+<?xml version="1.0" encoding="UTF-8"?>
+<testsuites>
+  <testsuite name="mesa-buildtest" tests="1">
+    <testcase classname="compile" name="error" status="fail" time="0">
+      <system-out>""" + str(e) + """</system-out>
+    </testcase>
+  </testsuite>
+</testsuites>""")
+        bs.Export().export_tests()
 
 if __name__ == '__main__':
     main()
