866c9da8271ec9744e6f00a163bd1376c4235326
Mark Janes <mark.a.janes@intel.com>
2015-04-08 12:51:35 -0700

    skip drm tests, which are longer than we care to run.

--- drm/build.py
+++ drm/build.py
@@ -5,6 +5,14 @@ import sys, os
 sys.path.append(os.path.join(os.path.dirname(os.path.abspath(sys.argv[0])), ".."))
 import build_support as bs
 
+class DrmBuilder(bs.AutoBuilder):
+    def __init__(self):
+        bs.AutoBuilder.__init__(self)
 
-bs.build(bs.AutoBuilder())
+    def test(self):
+        # libdrm now has a 2-minute long test, which is too long to
+        # wait for.
+        pass
+
+bs.build(DrmBuilder())
 
