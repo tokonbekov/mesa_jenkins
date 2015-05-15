ba126cdd08324d90ca7a1b7a3f98faa5325e9db5
Mark Janes <mark.a.janes@intel.com>
2015-05-15 15:51:36 -0700

    re-enable dma buf

--- piglit/build.py
+++ piglit/build.py
@@ -6,7 +6,7 @@ sys.path.append(os.path.join(os.path.dirname(os.path.abspath(sys.argv[0])), ".."
 import build_support as bs
 
 opts = [
-    '-DPIGLIT_BUILD_DMA_BUF_TESTS=0',
+    '-DPIGLIT_BUILD_DMA_BUF_TESTS=1',
     '-DPIGLIT_BUILD_GLES1_TESTS=1',
     '-DPIGLIT_BUILD_GLES2_TESTS=1',
     '-DPIGLIT_BUILD_GLES3_TESTS=1',
