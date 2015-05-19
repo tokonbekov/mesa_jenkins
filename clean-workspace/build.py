0cd46b2f3dede5ebb3d9da199b94e9ac33848b7a
Mark Janes <mark.a.janes@intel.com>
2015-05-19 13:08:28 -0700

    change the clean-workspace project to be a proper builder

--- clean-workspace/build.py
+++ clean-workspace/build.py
@@ -1,9 +1,24 @@
 #!/usr/bin/python
 
-import sys, os, argparse
+import sys, os
 sys.path.append(os.path.join(os.path.dirname(os.path.abspath(sys.argv[0])), ".."))
 import build_support as bs
 
-bs.run_batch_command(["git", "clean", "-xfd"])
-bs.run_batch_command(["git", "reset", "--hard", "HEAD"])
-bs.rmtree("repos")
+class Cleaner(object):
+    def __init__(self):
+        pass
+
+    def clean(self):
+        bs.run_batch_command(["git", "clean", "-xfd"])
+        bs.run_batch_command(["git", "reset", "--hard", "HEAD"])
+        bs.rmtree("repos")
+
+    def build(self):
+        pass
+
+    def test(self):
+        pass
+
+bs.build(Cleaner())
+
+        
