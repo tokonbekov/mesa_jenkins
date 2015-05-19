4bb269971b273fafef554edd4e20689ab71a4342
Mark Janes <mark.a.janes@intel.com>
2015-05-19 12:41:37 -0700

    add job to wipe workspace
    With the rename of bisect.py, the associated pyc was left in place on
    the build machines.  This new job allows us to set the workspace to a
    pristine state.  Ordinarily, this is undesirable, because it requires
    the builder to clone the repos again.

---
+++ clear_workspace/build.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python
+
+import sys, os, argparse
+sys.path.append(os.path.join(os.path.dirname(os.path.abspath(sys.argv[0])), ".."))
+import build_support as bs
+
+bs.run_batch_command(["git", "clean", "-xfd"])
+bs.run_batch_command(["git", "reset", "--hard", "HEAD"])
