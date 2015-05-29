faf7aaa97ff905f3645ef2a738c9c11808dfddad
Mark Janes <mark.a.janes@intel.com>
2015-05-29 13:48:57 -0700

    Fix system reboot
    The change to support concurrent builds required building projects
    within a user namespace.  However, `unshare -m -r` results in
    all file ownership going to 'nobody'.  This breaks sudo, and the
    reboot-slave component.
    
    This commit changes reboot-slave to trigger the reboot-single jenkins
    job, which does not suffer from the same limitations as Leeroy.

--- reboot-slave/build.py
+++ reboot-slave/build.py
@@ -1,12 +1,17 @@
 #!/usr/bin/python
 
-import sys, os, argparse
+import os
+import sys
+import urllib2
 sys.path.append(os.path.join(os.path.dirname(os.path.abspath(sys.argv[0])), ".."))
 import build_support as bs
 
-import time
+# Leeroy runs the builds inside of a user namespace, so it can bind
+# mount on /tmp.  This prevents the use of sudo.  This build just
+# triggers the "single_reboot" job, which does not run in a user
+# namespace.
 
-# add a sleep so the scheduler can dispatch all the jobs before the first one fails
-time.sleep(30)
-
-bs.run_batch_command(["sudo", "reboot"])
+server = bs.ProjectMap().build_spec().find("build_master").attrib["host"]
+url = "http://" + server + "/job/reboot_single/buildWithParameters?token=xyzzy&label=" + bs.Options().hardware
+print "opening: " + url
+urllib2.urlopen(url)
