6367a5190467aae7d113244663fbcaa85d8bb4eb
Mark Janes <mark.a.janes@intel.com>
2015-06-03 12:25:21 -0700

    drop bsw hanger again, so bisect can complete on other fixed tests

--- build_support/builders.py
+++ build_support/builders.py
@@ -368,7 +368,8 @@ class PiglitTester(object):
             pass
 
         if "bsw" in hardware:
-            pass
+            # bug 90830 (need to disable for bisect)
+            exclude_tests = exclude_tests + ["arb_gpu_shader5.execution.sampler_array_indexing"]
 
         for test in exclude_tests:
             fixed_test = test.replace('_', '.')
