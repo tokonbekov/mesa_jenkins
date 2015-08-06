c9c146ca42e0d56290201c7c4b355fed596e281f
Mark Janes <mark.a.janes@intel.com>
2015-08-06 15:15:47 -0700

    re-enable concurrent skl

--- build_support/builders.py
+++ build_support/builders.py
@@ -416,8 +416,6 @@ class PiglitTester(object):
             cmd = cmd + ["--include-tests", self._piglit_test]
             
         concurrency_options = ["-c"]
-        if "skl" in hardware and self.suite != "cpu":
-            concurrency_options = ["-1", "-v"]
         cmd = cmd + concurrency_options
             
         cmd = cmd + [self.suite,
