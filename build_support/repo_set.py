10bdf274ca5db833e442c408703f9b43925016ef
Mark Janes <mark.a.janes@intel.com>
2015-08-24 11:52:05 -0700

    be defensive about checking branch history
    dEQP has no origin/master

--- build_support/repo_set.py
+++ build_support/repo_set.py
@@ -189,7 +189,12 @@ class RepoSet:
 
             # branchs can be a long time in the past.  For 10.4, there
             # have been more than 1000 commits since the branch point.
-            for master_commit in repo.iter_commits('origin/master', max_count=4000):
+            commits = []
+            try:
+                commits = repo.iter_commits('origin/master', max_count=4000)
+            except(git.exc.GitCommandError):
+                continue
+            for master_commit in commits:
                 hexsha = master_commit.hexsha
                 if hexsha not in branch_commits:
                     tmp_revs.append(hexsha)
