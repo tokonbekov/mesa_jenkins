38e83034a0323433fbdf8799bace9514f99cccad
Mark Janes <mark.a.janes@intel.com>
2015-09-08 16:34:27 -0700

    drop intermittent skl test

--- build_support/builders.py
+++ build_support/builders.py
@@ -392,6 +392,8 @@ class PiglitTester(object):
             # hard hangs skl
             exclude_tests = exclude_tests + ["fbo-depth-array"]
             exclude_tests = exclude_tests + ["ext_framebuffer_multisample.no-color"]
+            # intermittent, bug 91926
+            exclude_tests = exclude_tests + ["spec.arb_pixel_buffer_object.texsubimage.pbo"]
 
         if "bsw" in hardware:
             # TODO: write bug
