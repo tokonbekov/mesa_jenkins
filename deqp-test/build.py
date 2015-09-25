53b0f69014c5a0dd13ee889732ef32c736b97370
Mark Janes <mark.a.janes@intel.com>
2015-09-25 08:48:05 -0700

    drop another dEQP flush test

--- deqp-test/build.py
+++ deqp-test/build.py
@@ -182,8 +182,9 @@ class DeqpBuilder:
                      "functional.shaders.random.conditionals.combined",
                      # fails intermittently on at least bdw and hsw
                      "functional.flush_finish.flush",
-                     "functional.flush_finish.finish"
-                     ]
+                     "functional.flush_finish.finish",
+                     "functional.flush_finish.finish_wait"]
+            
             if "snb" in o.hardware:
                 skips = skips + ["functional.shaders.random.texture.vertex.45",
                                  "functional.shaders.random.texture.vertex.1"]
