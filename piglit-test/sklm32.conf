yuq/mesa-lima

commit 6c731d85666abb61c49e5b4affa196545f5ac086

nir: Add a nir_deref_tail helper
Reviewed-by: Connor Abbott <cwabbott0@gmail.com>

src/glsl/nir/nir.h
@@ -785,6 +785,15 @@ NIR_DEFINE_CAST(nir_deref_as_var, nir_deref, nir_deref_var, deref)
 NIR_DEFINE_CAST(nir_deref_as_array, nir_deref, nir_deref_array, deref)
 NIR_DEFINE_CAST(nir_deref_as_struct, nir_deref, nir_deref_struct, deref)
 
+/* Returns the last deref in the chain. */
+static inline nir_deref *
+nir_deref_tail(nir_deref *deref)
+{
+   while (deref->child)
+      deref = deref->child;
+   return deref;
+}
+
 typedef struct {
    nir_instr instr;
 

src/glsl/nir/nir_lower_var_copies.c
@@ -53,17 +53,6 @@ deref_next_wildcard_parent(nir_deref *deref)
    return NULL;
 }
 
-/* Returns the last deref in the chain.
- */
-static nir_deref *
-get_deref_tail(nir_deref *deref)
-{
-   while (deref->child)
-      deref = deref->child;
-
-   return deref;
-}
-
 /* This function recursively walks the given deref chain and replaces the
  * given copy instruction with an equivalent sequence load/store
  * operations.
@@ -121,8 +110,8 @@ emit_copy_load_store(nir_intrinsic_instr *copy_instr,
    } else {
       /* In this case, we have no wildcards anymore, so all we have to do
        * is just emit the load and store operations. */
-      src_tail = get_deref_tail(src_tail);
-      dest_tail = get_deref_tail(dest_tail);
+      src_tail = nir_deref_tail(src_tail);
+      dest_tail = nir_deref_tail(dest_tail);
 
       assert(src_tail->type == dest_tail->type);
 

src/glsl/nir/nir_split_var_copies.c
@@ -67,14 +67,6 @@ struct split_var_copies_state {
    bool progress;
 };
 
-static nir_deref *
-get_deref_tail(nir_deref *deref)
-{
-   while (deref->child != NULL)
-      deref = deref->child;
-   return deref;
-}
-
 /* Recursively constructs deref chains to split a copy instruction into
  * multiple (if needed) copy instructions with full-length deref chains.
  * External callers of this function should pass the tail and head of the
@@ -227,8 +219,8 @@ split_var_copies_block(nir_block *block, void *void_state)
 
       nir_deref *dest_head = &intrinsic->variables[0]->deref;
       nir_deref *src_head = &intrinsic->variables[1]->deref;
-      nir_deref *dest_tail = get_deref_tail(dest_head);
-      nir_deref *src_tail = get_deref_tail(src_head);
+      nir_deref *dest_tail = nir_deref_tail(dest_head);
+      nir_deref *src_tail = nir_deref_tail(src_head);
 
       switch (glsl_get_base_type(src_tail->type)) {
       case GLSL_TYPE_ARRAY:

tokonbekov/mesa_jenkins

commit 468fb742107c7555d1e1c01736ee082ef379e399

regressions from 6c731d85666abb61c49e5b4affa196545f5ac086

piglit-test/bdw.conf
@@ -138,6 +138,16 @@ piglit.spec.arb_explicit_uniform_location.compiler.layout-subroutine-index.vert 
 piglit.spec.arb_shader_subroutine.compiler.layout-qualifier-index-invalid.vert = piglit 9b2087f
 piglit.spec.glsl-1_20.execution.vs-function-inout-array-self-assignment = piglit 9559cb5
 piglit.spec.arb_explicit_uniform_location.compiler.layout-subroutine-location-function.vert = piglit f3c2631
+piglit.spec.arb_shader_subroutine.execution.simple-subroutine = mesa 6c731d85666abb61c49e5b4affa196545f5ac086
+piglit.spec.arb_arrays_of_arrays.execution.subroutines.fs-subroutine = mesa 6c731d85666abb61c49e5b4affa196545f5ac086
+piglit.spec.arb_arrays_of_arrays.compiler.subroutine.vert = mesa 6c731d85666abb61c49e5b4affa196545f5ac086
+piglit.spec.arb_shader_subroutine.execution.array-subroutines-nonconst = mesa 6c731d85666abb61c49e5b4affa196545f5ac086
+piglit.spec.arb_shader_subroutine.execution.two-subroutines-2 = mesa 6c731d85666abb61c49e5b4affa196545f5ac086
+piglit.spec.arb_shader_subroutine.execution.two-subroutines-nested = mesa 6c731d85666abb61c49e5b4affa196545f5ac086
+piglit.spec.arb_program_interface_query.arb_program_interface_query-getprogramresourceiv = mesa 6c731d85666abb61c49e5b4affa196545f5ac086
+piglit.spec.arb_shader_subroutine.execution.array-subroutines = mesa 6c731d85666abb61c49e5b4affa196545f5ac086
+piglit.spec.arb_shader_subroutine.execution.two-subroutines-uniform = mesa 6c731d85666abb61c49e5b4affa196545f5ac086
+piglit.spec.arb_shader_subroutine.execution.two-subroutines = mesa 6c731d85666abb61c49e5b4affa196545f5ac086
 
 [fixed-tests]
 piglit.spec.ext_texture_integer.texwrap formats bordercolor-swizzled = mesa 08a06b6

piglit-test/bdwm32.conf
@@ -138,6 +138,16 @@ piglit.spec.arb_explicit_uniform_location.compiler.layout-subroutine-index.vert 
 piglit.spec.arb_shader_subroutine.compiler.layout-qualifier-index-invalid.vert = piglit 9b2087f
 piglit.spec.glsl-1_20.execution.vs-function-inout-array-self-assignment = piglit 9559cb5
 piglit.spec.arb_explicit_uniform_location.compiler.layout-subroutine-location-function.vert = piglit f3c2631
+piglit.spec.arb_shader_subroutine.execution.simple-subroutine = mesa 6c731d85666abb61c49e5b4affa196545f5ac086
+piglit.spec.arb_arrays_of_arrays.execution.subroutines.fs-subroutine = mesa 6c731d85666abb61c49e5b4affa196545f5ac086
+piglit.spec.arb_arrays_of_arrays.compiler.subroutine.vert = mesa 6c731d85666abb61c49e5b4affa196545f5ac086
+piglit.spec.arb_shader_subroutine.execution.array-subroutines-nonconst = mesa 6c731d85666abb61c49e5b4affa196545f5ac086
+piglit.spec.arb_shader_subroutine.execution.two-subroutines-2 = mesa 6c731d85666abb61c49e5b4affa196545f5ac086
+piglit.spec.arb_shader_subroutine.execution.two-subroutines-nested = mesa 6c731d85666abb61c49e5b4affa196545f5ac086
+piglit.spec.arb_program_interface_query.arb_program_interface_query-getprogramresourceiv = mesa 6c731d85666abb61c49e5b4affa196545f5ac086
+piglit.spec.arb_shader_subroutine.execution.array-subroutines = mesa 6c731d85666abb61c49e5b4affa196545f5ac086
+piglit.spec.arb_shader_subroutine.execution.two-subroutines-uniform = mesa 6c731d85666abb61c49e5b4affa196545f5ac086
+piglit.spec.arb_shader_subroutine.execution.two-subroutines = mesa 6c731d85666abb61c49e5b4affa196545f5ac086
 
 [fixed-tests]
 piglit.spec.ext_texture_integer.texwrap formats bordercolor = mesa 08a06b6

piglit-test/bsw.conf
@@ -133,6 +133,16 @@ piglit.spec.arb_shader_subroutine.compiler.layout-qualifier-index-invalid.vert =
 piglit.spec.arb_explicit_uniform_location.compiler.layout-subroutine-index.vert = piglit 9b2087f
 piglit.spec.glsl-1_20.execution.vs-function-inout-array-self-assignment = piglit 9559cb5
 piglit.spec.arb_explicit_uniform_location.compiler.layout-subroutine-location-function.vert = piglit f3c2631
+piglit.spec.arb_shader_subroutine.execution.simple-subroutine = mesa 6c731d85666abb61c49e5b4affa196545f5ac086
+piglit.spec.arb_arrays_of_arrays.execution.subroutines.fs-subroutine = mesa 6c731d85666abb61c49e5b4affa196545f5ac086
+piglit.spec.arb_arrays_of_arrays.compiler.subroutine.vert = mesa 6c731d85666abb61c49e5b4affa196545f5ac086
+piglit.spec.arb_shader_subroutine.execution.array-subroutines-nonconst = mesa 6c731d85666abb61c49e5b4affa196545f5ac086
+piglit.spec.arb_shader_subroutine.execution.two-subroutines-2 = mesa 6c731d85666abb61c49e5b4affa196545f5ac086
+piglit.spec.arb_shader_subroutine.execution.two-subroutines-nested = mesa 6c731d85666abb61c49e5b4affa196545f5ac086
+piglit.spec.arb_program_interface_query.arb_program_interface_query-getprogramresourceiv = mesa 6c731d85666abb61c49e5b4affa196545f5ac086
+piglit.spec.arb_shader_subroutine.execution.array-subroutines = mesa 6c731d85666abb61c49e5b4affa196545f5ac086
+piglit.spec.arb_shader_subroutine.execution.two-subroutines-uniform = mesa 6c731d85666abb61c49e5b4affa196545f5ac086
+piglit.spec.arb_shader_subroutine.execution.two-subroutines = mesa 6c731d85666abb61c49e5b4affa196545f5ac086
 
 [fixed-tests]
 piglit.shaders.glsl-getactiveuniform-count. glsl-getactiveuniform-mvp = mesa_87a4bc5

piglit-test/bswm32.conf
@@ -132,6 +132,16 @@ piglit.spec.arb_shader_subroutine.compiler.layout-qualifier-index-invalid.vert =
 piglit.spec.arb_explicit_uniform_location.compiler.layout-subroutine-index.vert = piglit 9b2087f
 piglit.spec.glsl-1_20.execution.vs-function-inout-array-self-assignment = piglit 9559cb5
 piglit.spec.arb_explicit_uniform_location.compiler.layout-subroutine-location-function.vert = piglit f3c2631
+piglit.spec.arb_shader_subroutine.execution.simple-subroutine = mesa 6c731d85666abb61c49e5b4affa196545f5ac086
+piglit.spec.arb_arrays_of_arrays.execution.subroutines.fs-subroutine = mesa 6c731d85666abb61c49e5b4affa196545f5ac086
+piglit.spec.arb_arrays_of_arrays.compiler.subroutine.vert = mesa 6c731d85666abb61c49e5b4affa196545f5ac086
+piglit.spec.arb_shader_subroutine.execution.array-subroutines-nonconst = mesa 6c731d85666abb61c49e5b4affa196545f5ac086
+piglit.spec.arb_shader_subroutine.execution.two-subroutines-2 = mesa 6c731d85666abb61c49e5b4affa196545f5ac086
+piglit.spec.arb_shader_subroutine.execution.two-subroutines-nested = mesa 6c731d85666abb61c49e5b4affa196545f5ac086
+piglit.spec.arb_program_interface_query.arb_program_interface_query-getprogramresourceiv = mesa 6c731d85666abb61c49e5b4affa196545f5ac086
+piglit.spec.arb_shader_subroutine.execution.array-subroutines = mesa 6c731d85666abb61c49e5b4affa196545f5ac086
+piglit.spec.arb_shader_subroutine.execution.two-subroutines-uniform = mesa 6c731d85666abb61c49e5b4affa196545f5ac086
+piglit.spec.arb_shader_subroutine.execution.two-subroutines = mesa 6c731d85666abb61c49e5b4affa196545f5ac086
 
 [fixed-tests]
 piglit.shaders.glsl-getactiveuniform-count. glsl-getactiveuniform-mvp = mesa_87a4bc5

piglit-test/byt.conf
@@ -208,6 +208,16 @@ piglit.spec.arb_explicit_uniform_location.compiler.layout-subroutine-index.vert 
 piglit.spec.arb_shader_subroutine.compiler.layout-qualifier-index-invalid.vert = piglit 9b2087f
 piglit.spec.glsl-1_20.execution.vs-function-inout-array-self-assignment = piglit 9559cb5
 piglit.spec.arb_explicit_uniform_location.compiler.layout-subroutine-location-function.vert = piglit f3c2631
+piglit.spec.arb_shader_subroutine.execution.simple-subroutine = mesa 6c731d85666abb61c49e5b4affa196545f5ac086
+piglit.spec.arb_arrays_of_arrays.execution.subroutines.fs-subroutine = mesa 6c731d85666abb61c49e5b4affa196545f5ac086
+piglit.spec.arb_arrays_of_arrays.compiler.subroutine.vert = mesa 6c731d85666abb61c49e5b4affa196545f5ac086
+piglit.spec.arb_shader_subroutine.execution.array-subroutines-nonconst = mesa 6c731d85666abb61c49e5b4affa196545f5ac086
+piglit.spec.arb_shader_subroutine.execution.two-subroutines-2 = mesa 6c731d85666abb61c49e5b4affa196545f5ac086
+piglit.spec.arb_shader_subroutine.execution.two-subroutines-nested = mesa 6c731d85666abb61c49e5b4affa196545f5ac086
+piglit.spec.arb_program_interface_query.arb_program_interface_query-getprogramresourceiv = mesa 6c731d85666abb61c49e5b4affa196545f5ac086
+piglit.spec.arb_shader_subroutine.execution.array-subroutines = mesa 6c731d85666abb61c49e5b4affa196545f5ac086
+piglit.spec.arb_shader_subroutine.execution.two-subroutines-uniform = mesa 6c731d85666abb61c49e5b4affa196545f5ac086
+piglit.spec.arb_shader_subroutine.execution.two-subroutines = mesa 6c731d85666abb61c49e5b4affa196545f5ac086
 
 [fixed-tests]
 piglit.spec.arb_occlusion_query.occlusion_query_meta_save = mesa bb77745681e179b53ea64da7bc1987b57643b7b0

piglit-test/bytm32.conf
@@ -208,6 +208,16 @@ piglit.spec.arb_explicit_uniform_location.compiler.layout-subroutine-index.vert 
 piglit.spec.arb_shader_subroutine.compiler.layout-qualifier-index-invalid.vert = piglit 9b2087f
 piglit.spec.glsl-1_20.execution.vs-function-inout-array-self-assignment = piglit 9559cb5
 piglit.spec.arb_explicit_uniform_location.compiler.layout-subroutine-location-function.vert = piglit f3c2631
+piglit.spec.arb_shader_subroutine.execution.simple-subroutine = mesa 6c731d85666abb61c49e5b4affa196545f5ac086
+piglit.spec.arb_arrays_of_arrays.execution.subroutines.fs-subroutine = mesa 6c731d85666abb61c49e5b4affa196545f5ac086
+piglit.spec.arb_arrays_of_arrays.compiler.subroutine.vert = mesa 6c731d85666abb61c49e5b4affa196545f5ac086
+piglit.spec.arb_shader_subroutine.execution.array-subroutines-nonconst = mesa 6c731d85666abb61c49e5b4affa196545f5ac086
+piglit.spec.arb_shader_subroutine.execution.two-subroutines-2 = mesa 6c731d85666abb61c49e5b4affa196545f5ac086
+piglit.spec.arb_shader_subroutine.execution.two-subroutines-nested = mesa 6c731d85666abb61c49e5b4affa196545f5ac086
+piglit.spec.arb_program_interface_query.arb_program_interface_query-getprogramresourceiv = mesa 6c731d85666abb61c49e5b4affa196545f5ac086
+piglit.spec.arb_shader_subroutine.execution.array-subroutines = mesa 6c731d85666abb61c49e5b4affa196545f5ac086
+piglit.spec.arb_shader_subroutine.execution.two-subroutines-uniform = mesa 6c731d85666abb61c49e5b4affa196545f5ac086
+piglit.spec.arb_shader_subroutine.execution.two-subroutines = mesa 6c731d85666abb61c49e5b4affa196545f5ac086
 
 [fixed-tests]
 piglit.spec.arb_occlusion_query.occlusion_query_meta_save = mesa bb77745681e179b53ea64da7bc1987b57643b7b0

piglit-test/hsw.conf
@@ -139,6 +139,16 @@ piglit.spec.arb_explicit_uniform_location.compiler.layout-subroutine-index.vert 
 piglit.spec.arb_shader_subroutine.compiler.layout-qualifier-index-invalid.vert = piglit 9b2087f
 piglit.spec.glsl-1_20.execution.vs-function-inout-array-self-assignment = piglit 9559cb5
 piglit.spec.arb_explicit_uniform_location.compiler.layout-subroutine-location-function.vert = piglit f3c2631
+piglit.spec.arb_shader_subroutine.execution.simple-subroutine = mesa 6c731d85666abb61c49e5b4affa196545f5ac086
+piglit.spec.arb_arrays_of_arrays.execution.subroutines.fs-subroutine = mesa 6c731d85666abb61c49e5b4affa196545f5ac086
+piglit.spec.arb_arrays_of_arrays.compiler.subroutine.vert = mesa 6c731d85666abb61c49e5b4affa196545f5ac086
+piglit.spec.arb_shader_subroutine.execution.array-subroutines-nonconst = mesa 6c731d85666abb61c49e5b4affa196545f5ac086
+piglit.spec.arb_shader_subroutine.execution.two-subroutines-2 = mesa 6c731d85666abb61c49e5b4affa196545f5ac086
+piglit.spec.arb_shader_subroutine.execution.two-subroutines-nested = mesa 6c731d85666abb61c49e5b4affa196545f5ac086
+piglit.spec.arb_program_interface_query.arb_program_interface_query-getprogramresourceiv = mesa 6c731d85666abb61c49e5b4affa196545f5ac086
+piglit.spec.arb_shader_subroutine.execution.array-subroutines = mesa 6c731d85666abb61c49e5b4affa196545f5ac086
+piglit.spec.arb_shader_subroutine.execution.two-subroutines-uniform = mesa 6c731d85666abb61c49e5b4affa196545f5ac086
+piglit.spec.arb_shader_subroutine.execution.two-subroutines = mesa 6c731d85666abb61c49e5b4affa196545f5ac086
 
 [fixed-tests]
 piglit.spec.arb_texture_rg.texwrap formats-int bordercolor = mesa 08a06b6

piglit-test/hswm32.conf
@@ -139,6 +139,16 @@ piglit.spec.arb_explicit_uniform_location.compiler.layout-subroutine-index.vert 
 piglit.spec.arb_shader_subroutine.compiler.layout-qualifier-index-invalid.vert = piglit 9b2087f
 piglit.spec.glsl-1_20.execution.vs-function-inout-array-self-assignment = piglit 9559cb5
 piglit.spec.arb_explicit_uniform_location.compiler.layout-subroutine-location-function.vert = piglit f3c2631
+piglit.spec.arb_shader_subroutine.execution.simple-subroutine = mesa 6c731d85666abb61c49e5b4affa196545f5ac086
+piglit.spec.arb_arrays_of_arrays.execution.subroutines.fs-subroutine = mesa 6c731d85666abb61c49e5b4affa196545f5ac086
+piglit.spec.arb_arrays_of_arrays.compiler.subroutine.vert = mesa 6c731d85666abb61c49e5b4affa196545f5ac086
+piglit.spec.arb_shader_subroutine.execution.array-subroutines-nonconst = mesa 6c731d85666abb61c49e5b4affa196545f5ac086
+piglit.spec.arb_shader_subroutine.execution.two-subroutines-2 = mesa 6c731d85666abb61c49e5b4affa196545f5ac086
+piglit.spec.arb_shader_subroutine.execution.two-subroutines-nested = mesa 6c731d85666abb61c49e5b4affa196545f5ac086
+piglit.spec.arb_program_interface_query.arb_program_interface_query-getprogramresourceiv = mesa 6c731d85666abb61c49e5b4affa196545f5ac086
+piglit.spec.arb_shader_subroutine.execution.array-subroutines = mesa 6c731d85666abb61c49e5b4affa196545f5ac086
+piglit.spec.arb_shader_subroutine.execution.two-subroutines-uniform = mesa 6c731d85666abb61c49e5b4affa196545f5ac086
+piglit.spec.arb_shader_subroutine.execution.two-subroutines = mesa 6c731d85666abb61c49e5b4affa196545f5ac086
 
 [fixed-tests]
 piglit.spec.ext_texture_integer.texwrap formats bordercolor-swizzled = mesa 08a06b6

piglit-test/ivb.conf
@@ -184,6 +184,16 @@ piglit.spec.arb_explicit_uniform_location.compiler.layout-subroutine-index.vert 
 piglit.spec.arb_shader_subroutine.compiler.layout-qualifier-index-invalid.vert = piglit 9b2087f
 piglit.spec.glsl-1_20.execution.vs-function-inout-array-self-assignment = piglit 9559cb5
 piglit.spec.arb_explicit_uniform_location.compiler.layout-subroutine-location-function.vert = piglit f3c2631
+piglit.spec.arb_shader_subroutine.execution.simple-subroutine = mesa 6c731d85666abb61c49e5b4affa196545f5ac086
+piglit.spec.arb_arrays_of_arrays.execution.subroutines.fs-subroutine = mesa 6c731d85666abb61c49e5b4affa196545f5ac086
+piglit.spec.arb_arrays_of_arrays.compiler.subroutine.vert = mesa 6c731d85666abb61c49e5b4affa196545f5ac086
+piglit.spec.arb_shader_subroutine.execution.array-subroutines-nonconst = mesa 6c731d85666abb61c49e5b4affa196545f5ac086
+piglit.spec.arb_shader_subroutine.execution.two-subroutines-2 = mesa 6c731d85666abb61c49e5b4affa196545f5ac086
+piglit.spec.arb_shader_subroutine.execution.two-subroutines-nested = mesa 6c731d85666abb61c49e5b4affa196545f5ac086
+piglit.spec.arb_program_interface_query.arb_program_interface_query-getprogramresourceiv = mesa 6c731d85666abb61c49e5b4affa196545f5ac086
+piglit.spec.arb_shader_subroutine.execution.array-subroutines = mesa 6c731d85666abb61c49e5b4affa196545f5ac086
+piglit.spec.arb_shader_subroutine.execution.two-subroutines-uniform = mesa 6c731d85666abb61c49e5b4affa196545f5ac086
+piglit.spec.arb_shader_subroutine.execution.two-subroutines = mesa 6c731d85666abb61c49e5b4affa196545f5ac086
 
 [fixed-tests]
 piglit.spec.arb_occlusion_query.occlusion_query_meta_save = mesa bb77745681e179b53ea64da7bc1987b57643b7b0

piglit-test/ivbm32.conf
@@ -183,6 +183,16 @@ piglit.spec.arb_explicit_uniform_location.compiler.layout-subroutine-index.vert 
 piglit.spec.arb_shader_subroutine.compiler.layout-qualifier-index-invalid.vert = piglit 9b2087f
 piglit.spec.glsl-1_20.execution.vs-function-inout-array-self-assignment = piglit 9559cb5
 piglit.spec.arb_explicit_uniform_location.compiler.layout-subroutine-location-function.vert = piglit f3c2631
+piglit.spec.arb_shader_subroutine.execution.simple-subroutine = mesa 6c731d85666abb61c49e5b4affa196545f5ac086
+piglit.spec.arb_arrays_of_arrays.execution.subroutines.fs-subroutine = mesa 6c731d85666abb61c49e5b4affa196545f5ac086
+piglit.spec.arb_arrays_of_arrays.compiler.subroutine.vert = mesa 6c731d85666abb61c49e5b4affa196545f5ac086
+piglit.spec.arb_shader_subroutine.execution.array-subroutines-nonconst = mesa 6c731d85666abb61c49e5b4affa196545f5ac086
+piglit.spec.arb_shader_subroutine.execution.two-subroutines-2 = mesa 6c731d85666abb61c49e5b4affa196545f5ac086
+piglit.spec.arb_shader_subroutine.execution.two-subroutines-nested = mesa 6c731d85666abb61c49e5b4affa196545f5ac086
+piglit.spec.arb_program_interface_query.arb_program_interface_query-getprogramresourceiv = mesa 6c731d85666abb61c49e5b4affa196545f5ac086
+piglit.spec.arb_shader_subroutine.execution.array-subroutines = mesa 6c731d85666abb61c49e5b4affa196545f5ac086
+piglit.spec.arb_shader_subroutine.execution.two-subroutines-uniform = mesa 6c731d85666abb61c49e5b4affa196545f5ac086
+piglit.spec.arb_shader_subroutine.execution.two-subroutines = mesa 6c731d85666abb61c49e5b4affa196545f5ac086
 
 [fixed-tests]
 piglit.spec.arb_occlusion_query.occlusion_query_meta_save = mesa bb77745681e179b53ea64da7bc1987b57643b7b0

piglit-test/skl.conf
@@ -138,6 +138,16 @@ piglit.spec.arb_explicit_uniform_location.compiler.layout-subroutine-index.vert 
 piglit.spec.arb_shader_subroutine.compiler.layout-qualifier-index-invalid.vert = piglit 9b2087f
 piglit.spec.glsl-1_20.execution.vs-function-inout-array-self-assignment = piglit 9559cb5
 piglit.spec.arb_explicit_uniform_location.compiler.layout-subroutine-location-function.vert = piglit f3c2631
+piglit.spec.arb_shader_subroutine.execution.simple-subroutine = mesa 6c731d85666abb61c49e5b4affa196545f5ac086
+piglit.spec.arb_arrays_of_arrays.execution.subroutines.fs-subroutine = mesa 6c731d85666abb61c49e5b4affa196545f5ac086
+piglit.spec.arb_arrays_of_arrays.compiler.subroutine.vert = mesa 6c731d85666abb61c49e5b4affa196545f5ac086
+piglit.spec.arb_shader_subroutine.execution.array-subroutines-nonconst = mesa 6c731d85666abb61c49e5b4affa196545f5ac086
+piglit.spec.arb_shader_subroutine.execution.two-subroutines-2 = mesa 6c731d85666abb61c49e5b4affa196545f5ac086
+piglit.spec.arb_shader_subroutine.execution.two-subroutines-nested = mesa 6c731d85666abb61c49e5b4affa196545f5ac086
+piglit.spec.arb_program_interface_query.arb_program_interface_query-getprogramresourceiv = mesa 6c731d85666abb61c49e5b4affa196545f5ac086
+piglit.spec.arb_shader_subroutine.execution.array-subroutines = mesa 6c731d85666abb61c49e5b4affa196545f5ac086
+piglit.spec.arb_shader_subroutine.execution.two-subroutines-uniform = mesa 6c731d85666abb61c49e5b4affa196545f5ac086
+piglit.spec.arb_shader_subroutine.execution.two-subroutines = mesa 6c731d85666abb61c49e5b4affa196545f5ac086
 
 [fixed-tests]
 piglit.spec.glsl-es-3_00.compiler.uniform_block.interface-name-basic.vert = fixed_after_10.5_49299ef6fa0cf88e7101512400a4e74efa300941

piglit-test/sklm32.conf
@@ -139,6 +139,16 @@ piglit.spec.arb_explicit_uniform_location.compiler.layout-subroutine-index.vert 
 piglit.spec.arb_shader_subroutine.compiler.layout-qualifier-index-invalid.vert = piglit 9b2087f
 piglit.spec.glsl-1_20.execution.vs-function-inout-array-self-assignment = piglit 9559cb5
 piglit.spec.arb_explicit_uniform_location.compiler.layout-subroutine-location-function.vert = piglit f3c2631
+piglit.spec.arb_shader_subroutine.execution.simple-subroutine = mesa 6c731d85666abb61c49e5b4affa196545f5ac086
+piglit.spec.arb_arrays_of_arrays.execution.subroutines.fs-subroutine = mesa 6c731d85666abb61c49e5b4affa196545f5ac086
+piglit.spec.arb_arrays_of_arrays.compiler.subroutine.vert = mesa 6c731d85666abb61c49e5b4affa196545f5ac086
+piglit.spec.arb_shader_subroutine.execution.array-subroutines-nonconst = mesa 6c731d85666abb61c49e5b4affa196545f5ac086
+piglit.spec.arb_shader_subroutine.execution.two-subroutines-2 = mesa 6c731d85666abb61c49e5b4affa196545f5ac086
+piglit.spec.arb_shader_subroutine.execution.two-subroutines-nested = mesa 6c731d85666abb61c49e5b4affa196545f5ac086
+piglit.spec.arb_program_interface_query.arb_program_interface_query-getprogramresourceiv = mesa 6c731d85666abb61c49e5b4affa196545f5ac086
+piglit.spec.arb_shader_subroutine.execution.array-subroutines = mesa 6c731d85666abb61c49e5b4affa196545f5ac086
+piglit.spec.arb_shader_subroutine.execution.two-subroutines-uniform = mesa 6c731d85666abb61c49e5b4affa196545f5ac086
+piglit.spec.arb_shader_subroutine.execution.two-subroutines = mesa 6c731d85666abb61c49e5b4affa196545f5ac086
 
 [fixed-tests]
 piglit.spec.glsl-es-3_00.compiler.uniform_block.interface-name-basic.vert = fixed_after_10.5_49299ef6fa0cf88e7101512400a4e74efa300941

piglit-test/snb.conf
@@ -168,6 +168,15 @@ piglit.spec.arb_explicit_uniform_location.compiler.layout-subroutine-index.vert 
 piglit.spec.arb_shader_subroutine.compiler.layout-qualifier-index-invalid.vert = piglit 9b2087f
 piglit.spec.glsl-1_20.execution.vs-function-inout-array-self-assignment = piglit 9559cb5
 piglit.spec.arb_explicit_uniform_location.compiler.layout-subroutine-location-function.vert = piglit f3c2631
+piglit.spec.arb_shader_subroutine.execution.simple-subroutine = mesa 6c731d85666abb61c49e5b4affa196545f5ac086
+piglit.spec.arb_arrays_of_arrays.execution.subroutines.fs-subroutine = mesa 6c731d85666abb61c49e5b4affa196545f5ac086
+piglit.spec.arb_shader_subroutine.execution.array-subroutines-nonconst = mesa 6c731d85666abb61c49e5b4affa196545f5ac086
+piglit.spec.arb_shader_subroutine.execution.two-subroutines-2 = mesa 6c731d85666abb61c49e5b4affa196545f5ac086
+piglit.spec.arb_shader_subroutine.execution.two-subroutines-nested = mesa 6c731d85666abb61c49e5b4affa196545f5ac086
+piglit.spec.arb_program_interface_query.arb_program_interface_query-getprogramresourceiv = mesa 6c731d85666abb61c49e5b4affa196545f5ac086
+piglit.spec.arb_shader_subroutine.execution.array-subroutines = mesa 6c731d85666abb61c49e5b4affa196545f5ac086
+piglit.spec.arb_shader_subroutine.execution.two-subroutines-uniform = mesa 6c731d85666abb61c49e5b4affa196545f5ac086
+piglit.spec.arb_shader_subroutine.execution.two-subroutines = mesa 6c731d85666abb61c49e5b4affa196545f5ac086
 
 [fixed-tests]
 piglit.spec.arb_occlusion_query.occlusion_query_meta_save = mesa bb77745681e179b53ea64da7bc1987b57643b7b0

piglit-test/snbm32.conf
@@ -168,6 +168,15 @@ piglit.spec.arb_explicit_uniform_location.compiler.layout-subroutine-index.vert 
 piglit.spec.arb_shader_subroutine.compiler.layout-qualifier-index-invalid.vert = piglit 9b2087f
 piglit.spec.glsl-1_20.execution.vs-function-inout-array-self-assignment = piglit 9559cb5
 piglit.spec.arb_explicit_uniform_location.compiler.layout-subroutine-location-function.vert = piglit f3c2631
+piglit.spec.arb_shader_subroutine.execution.simple-subroutine = mesa 6c731d85666abb61c49e5b4affa196545f5ac086
+piglit.spec.arb_arrays_of_arrays.execution.subroutines.fs-subroutine = mesa 6c731d85666abb61c49e5b4affa196545f5ac086
+piglit.spec.arb_shader_subroutine.execution.array-subroutines-nonconst = mesa 6c731d85666abb61c49e5b4affa196545f5ac086
+piglit.spec.arb_shader_subroutine.execution.two-subroutines-2 = mesa 6c731d85666abb61c49e5b4affa196545f5ac086
+piglit.spec.arb_shader_subroutine.execution.two-subroutines-nested = mesa 6c731d85666abb61c49e5b4affa196545f5ac086
+piglit.spec.arb_program_interface_query.arb_program_interface_query-getprogramresourceiv = mesa 6c731d85666abb61c49e5b4affa196545f5ac086
+piglit.spec.arb_shader_subroutine.execution.array-subroutines = mesa 6c731d85666abb61c49e5b4affa196545f5ac086
+piglit.spec.arb_shader_subroutine.execution.two-subroutines-uniform = mesa 6c731d85666abb61c49e5b4affa196545f5ac086
+piglit.spec.arb_shader_subroutine.execution.two-subroutines = mesa 6c731d85666abb61c49e5b4affa196545f5ac086
 
 [fixed-tests]
 piglit.spec.arb_occlusion_query.occlusion_query_meta_save = mesa bb77745681e179b53ea64da7bc1987b57643b7b0