e6c1acfbb8bb8a5c775a098a295bf57ed605a6a2
Mark Janes <mark.a.janes@intel.com>
2015-12-14 09:18:13 -0800

    re-enable fixed tests

--- build_support/builders.py
+++ build_support/builders.py
@@ -443,11 +443,6 @@ class PiglitTester(object):
             exclude_tests = exclude_tests + ["arb_shader_storage_buffer_object.layout-std140-write-shader",
                                              "arb_shader_storage_buffer_object.layout-std430-write-shader"]
 
-            # bug 92822
-            exclude_tests = exclude_tests + ["arb_arrays_of_arrays.execution.image_store.basic-imagestore-non-const-uniform-index",
-                                             "arb_arrays_of_arrays.execution.image_store.basic-imagestore-const-uniform-index",
-                                             "arb_arrays_of_arrays.execution.image_store.basic-imagestore-mixed-const-non-const-uniform-index"]
-        
         if "byt" in hardware:
             # bug 89219, fixed in 10c82c6c5fc415d323a5e9c6acdc6a4c85d6b712
             # exclude_tests = exclude_tests + ["arb_uniform_buffer_object.bufferstorage"]
