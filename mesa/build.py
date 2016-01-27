cc347ac2db94c51dbcec17fef4414a6fd7e68774
Mark Janes <mark.a.janes@intel.com>
2016-01-27 09:51:48 -0800

    fix filename on glsl test check

--- mesa/build.py
+++ mesa/build.py
@@ -1,4 +1,4 @@
-#!/usr/bin/python
+ #!/usr/bin/python
 
 import sys
 import os.path as path
@@ -58,7 +58,7 @@ class MesaBuilder(bs.AutoBuilder):
     def test(self):
         # override the test method, because we can't know exactly
         # where the tests will be as of 11.2
-        if path.exists(self._src_dir + "/src/glsl/tests/general-ir-test.cpp"):
+        if path.exists(self._src_dir + "/src/glsl/tests/general_ir_test.cpp"):
             self.AddGtests(["src/glsl/tests/general-ir-test",
                             "src/glsl/tests/sampler-types-test",
                             "src/glsl/tests/uniform-initializer-test"])
