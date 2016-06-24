f73d82b404c94a688641fda45db92631ae5cdaeb
Mark Janes <mark.a.janes@intel.com>
2016-06-24 15:59:53 -0700

    Add perf build between existing data points
    When --revision is passed a rev_a:rev_b string, it will generate a perf
    point for the commit between the two revisions.  If rev_b is missing, a
    perf point will be built for mesa master.

--- scripts/build_perf.py
+++ scripts/build_perf.py
@@ -1,10 +1,8 @@
 import argparse
-import datetime
 import git
 import os
 import random
 import sys
-import time
 sys.path.append(os.path.join(os.path.dirname(os.path.abspath(sys.argv[0])), ".."))
 import build_support as bs
 
@@ -28,6 +26,32 @@ def main():
         projects = args.project.split(",")
     revision = args.revision
 
+    bspec = bs.BuildSpecification()
+    bspec.checkout("mesa_perf")
+    mesa_repo = git.Repo(bs.ProjectMap().project_source_dir("mesa"))
+
+    if ":" in revision:
+        (start_rev, end_rev) = revision.split(":")
+        if not end_rev:
+            # user selected the last point in a plot.  Build current master
+            revision = "mesa=" + mesa_repo.git.rev_parse("HEAD", short=True)
+        elif not start_rev:
+            print "ERROR: user-generated perf builds cannot add older data points to the plot"
+            sys.exit(-1)
+        else:
+            commits = []
+            start_commit = mesa_repo.commit(start_rev)
+            found = False
+            for commit in mesa_repo.iter_commits(end_rev, max_count=8000):
+                if commit == start_commit:
+                    found = True
+                    break
+                commits.append(commit.hexsha)
+            if not found:
+                print "ERROR: " + start_rev + " not found in history of " + end_rev
+                sys.exit(-1)
+            revision = "mesa=" + commits[len(commits)/2]
+
     # some build_local params are not handled by the Options, which is
     # used by other modules.  This code strips out incompatible args
     o = bs.Options(["bogus"])
@@ -37,15 +61,12 @@ def main():
     o.__dict__.update(vdict)
     sys.argv = ["bogus"] + o.to_list()
 
-    bspec = bs.BuildSpecification()
-
     pm = bs.ProjectMap()
     bs.rmtree(pm.source_root() + "/test_summary.txt")
 
     # checkout the desired revision on top of recent revisions
     if not revision:
         # randomly select a commit post 11.2
-        mesa_repo = git.Repo(bs.ProjectMap().project_source_dir("mesa"))
         branch_commit = mesa_repo.tags["11.2-branchpoint"].commit.hexsha
         commits = []
         for commit in mesa_repo.iter_commits('origin/master', max_count=8000):
@@ -54,7 +75,6 @@ def main():
             commits.append(commit.hexsha)
         revision = "mesa=" + str(commits[int(random.random() * len(commits))])
         
-    bspec.checkout("mesa_perf")
     revspec = bs.RevisionSpecification(from_cmd_line=revision.split())
     revspec.checkout()
 
