3e8d4af1b6bf2478779081c8a8c64ac276bff7d0
Mark Janes <mark.a.janes@intel.com>
2016-07-22 12:36:44 -0700

    enable tess and tess_o benchmarks

---
+++ tess/build.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python
+
+import os
+import sys
+import os.path as path
+sys.path.append(path.join(path.dirname(path.abspath(sys.argv[0])), ".."))
+import build_support as bs
+
+
+bs.build(bs.PerfBuilder("tess", iterations=5))
+
