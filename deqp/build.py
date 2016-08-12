37bae64ba5627ad9c905a739dfb6ad0d24f33ec1
Mark Janes <mark.a.janes@intel.com>
2016-08-12 12:07:17 -0700

    Disable EGL testing in dEQP
    Inadequate multithreaded EGL handling results in intermittent dEQP
    failures for random tests.  CI cannot filter this type of test failure,
    so EGL must be disabled.

--- deqp/build.py
+++ deqp/build.py
@@ -4,5 +4,5 @@ import sys, os
 sys.path.append(os.path.join(os.path.dirname(os.path.abspath(sys.argv[0])), ".."))
 import build_support as bs
 
-bs.build(bs.CMakeBuilder(extra_definitions=["-DDEQP_TARGET=x11_egl"]))
+bs.build(bs.CMakeBuilder(extra_definitions=["-DDEQP_TARGET=x11_glx"]))
 
