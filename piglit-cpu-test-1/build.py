f2c12dd42d9fad5282107a579188c03a91a65c2a
Mark Janes <mark.a.janes@intel.com>
2016-10-15 14:43:45 -0700

    break piglit-cpu-test into equal portions
    This is now the long pole for the standard test matrix.

--- piglit-cpu-test-1/build.py
+++ piglit-cpu-test-1/build.py
@@ -13,11 +13,6 @@ import build_support as bs
 
 start_time = time.time()
 
-bs.build(bs.PiglitTester(_suite="cpu", device_override="byt"))
-bs.build(bs.PiglitTester(_suite="cpu", device_override="g45"))
-bs.build(bs.PiglitTester(_suite="cpu", device_override="g965"))
-bs.build(bs.PiglitTester(_suite="cpu", device_override="ilk"))
-bs.build(bs.PiglitTester(_suite="cpu", device_override="ivbgt2"))
 bs.build(bs.PiglitTester(_suite="cpu", device_override="snbgt2"))
 bs.build(bs.PiglitTester(_suite="cpu", device_override="hswgt3"))
 bs.build(bs.PiglitTester(_suite="cpu", device_override="bdwgt2"))
---
+++ piglit-cpu-test-2/build.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python
+
+import os
+import sys
+import time
+sys.path.append(os.path.join(os.path.dirname(os.path.abspath(sys.argv[0])), ".."))
+import build_support as bs
+
+# HACK
+# repeatedly invoking build() will overwrite the start times.  Record
+# it now and write the correct value when the last build() has
+# finished.  build() was not intended to be used this way.
+
+start_time = time.time()
+
+bs.build(bs.PiglitTester(_suite="cpu", device_override="byt"))
+bs.build(bs.PiglitTester(_suite="cpu", device_override="g45"))
+bs.build(bs.PiglitTester(_suite="cpu", device_override="g965"))
+bs.build(bs.PiglitTester(_suite="cpu", device_override="ilk"))
+bs.build(bs.PiglitTester(_suite="cpu", device_override="ivbgt2"))
+
+options = bs.Options()
+if (options.result_path):
+    bs.ProjectInvoke(options).set_info("start_time", start_time)
