e0cdb0907d4cfd6e4055069627e20637995f864f
Mark Janes <mark.a.janes@intel.com>
2016-10-26 20:25:25 -0700

    filter missing revisions from the default branch
    The default branch is not always origin/master.  This oversight led to
    improperly filtered tests fixed in the vulkan cts.

--- build_support/repo_set.py
+++ build_support/repo_set.py
@@ -266,7 +266,7 @@ class RepoSet:
             # branchs can be a long time in the past.  For 10.4, there
             # have been more than 1000 commits since the branch point.
             try:
-                for master_commit in repo.iter_commits('origin/master', max_count=8000):
+                for master_commit in repo.iter_commits(self._branches[project], max_count=8000):
                     hexsha = master_commit.hexsha
                     if hexsha not in branch_commits:
                         tmp_revs.append(hexsha)
