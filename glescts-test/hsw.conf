yuq/mesa-lima

commit ddc32537d6db69198e88ef0dfe19770bf9daa536

mesa: clamp viewport values only once when using glViewport()
It's useless to clamp the same values for all viewports.

+7% in the "viewport change" test (drawoverhead benchmark).

v2: - call clamp_viewport() in all callers of set_viewport_no_notify()

Signed-off-by: Samuel Pitoiset <samuel.pitoiset@gmail.com>
Reviewed-by: Nicolai Hähnle <nicolai.haehnle@amd.com> (v1)

src/mesa/main/viewport.c
@@ -36,13 +36,12 @@
 #include "viewport.h"
 
 static void
-set_viewport_no_notify(struct gl_context *ctx, unsigned idx,
-                       GLfloat x, GLfloat y,
-                       GLfloat width, GLfloat height)
+clamp_viewport(struct gl_context *ctx, GLfloat *x, GLfloat *y,
+               GLfloat *width, GLfloat *height)
 {
    /* clamp width and height to the implementation dependent range */
-   width  = MIN2(width, (GLfloat) ctx->Const.MaxViewportWidth);
-   height = MIN2(height, (GLfloat) ctx->Const.MaxViewportHeight);
+   *width  = MIN2(*width, (GLfloat) ctx->Const.MaxViewportWidth);
+   *height = MIN2(*height, (GLfloat) ctx->Const.MaxViewportHeight);
 
    /* The GL_ARB_viewport_array spec says:
     *
@@ -55,12 +54,18 @@ set_viewport_no_notify(struct gl_context *ctx, unsigned idx,
    if (ctx->Extensions.ARB_viewport_array ||
        (ctx->Extensions.OES_viewport_array &&
         _mesa_is_gles31(ctx))) {
-      x = CLAMP(x,
-                ctx->Const.ViewportBounds.Min, ctx->Const.ViewportBounds.Max);
-      y = CLAMP(y,
-                ctx->Const.ViewportBounds.Min, ctx->Const.ViewportBounds.Max);
+      *x = CLAMP(*x,
+                 ctx->Const.ViewportBounds.Min, ctx->Const.ViewportBounds.Max);
+      *y = CLAMP(*y,
+                 ctx->Const.ViewportBounds.Min, ctx->Const.ViewportBounds.Max);
    }
+}
 
+static void
+set_viewport_no_notify(struct gl_context *ctx, unsigned idx,
+                       GLfloat x, GLfloat y,
+                       GLfloat width, GLfloat height)
+{
    if (ctx->ViewportArray[idx].X == x &&
        ctx->ViewportArray[idx].Width == width &&
        ctx->ViewportArray[idx].Y == y &&
@@ -89,6 +94,10 @@ static void
 viewport(struct gl_context *ctx, GLint x, GLint y, GLsizei width,
          GLsizei height)
 {
+   /* Clamp the viewport to the implementation dependent values. */
+   clamp_viewport(ctx, (GLfloat *)&x, (GLfloat *)&y,
+                  (GLfloat *)&width, (GLfloat *)&height);
+
    /* The GL_ARB_viewport_array spec says:
     *
     *     "Viewport sets the parameters for all viewports to the same values
@@ -153,6 +162,7 @@ void
 _mesa_set_viewport(struct gl_context *ctx, unsigned idx, GLfloat x, GLfloat y,
                     GLfloat width, GLfloat height)
 {
+   clamp_viewport(ctx, &x, &y, &width, &height);
    set_viewport_no_notify(ctx, idx, x, y, width, height);
 
    if (ctx->Driver.Viewport)
@@ -161,9 +171,12 @@ _mesa_set_viewport(struct gl_context *ctx, unsigned idx, GLfloat x, GLfloat y,
 
 static void
 viewport_array(struct gl_context *ctx, GLuint first, GLsizei count,
-               const struct gl_viewport_inputs *inputs)
+               struct gl_viewport_inputs *inputs)
 {
    for (GLsizei i = 0; i < count; i++) {
+      clamp_viewport(ctx, &inputs[i].X, &inputs[i].Y,
+                     &inputs[i].Width, &inputs[i].Height);
+
       set_viewport_no_notify(ctx, i + first, inputs[i].X, inputs[i].Y,
                              inputs[i].Width, inputs[i].Height);
    }
@@ -177,7 +190,7 @@ _mesa_ViewportArrayv_no_error(GLuint first, GLsizei count, const GLfloat *v)
 {
    GET_CURRENT_CONTEXT(ctx);
 
-   const struct gl_viewport_inputs *const p = (struct gl_viewport_inputs *)v;
+   struct gl_viewport_inputs *p = (struct gl_viewport_inputs *)v;
    viewport_array(ctx, first, count, p);
 }
 
@@ -185,7 +198,7 @@ void GLAPIENTRY
 _mesa_ViewportArrayv(GLuint first, GLsizei count, const GLfloat *v)
 {
    int i;
-   const struct gl_viewport_inputs *const p = (struct gl_viewport_inputs *) v;
+   struct gl_viewport_inputs *p = (struct gl_viewport_inputs *)v;
    GET_CURRENT_CONTEXT(ctx);
 
    if (MESA_VERBOSE & VERBOSE_API)

tokonbekov/mesa_jenkins

commit 509cb0f2a62e38ff2b73ef212b7e137a87780e2d

mesa jenkins updates due to mesa=ddc32537d6d https://bugs.freedesktop.org/show_bug.cgi?id=101989 Regressed by series ending in ddc32537d6db69198e88ef0dfe19770bf9daa536 Author:     Samuel Pitoiset <samuel.pitoiset@gmail.com> mesa: clamp viewport values only once when using glViewport()

glescts-test/bdw.conf
@@ -4410,6 +4410,11 @@ ES32-CTS.functional.tessellation_geometry_interaction.render.passthrough.tessell
 ES32-CTS.functional.tessellation_geometry_interaction.render.scatter.geometry_scatter_instances
 ES32-CTS.functional.tessellation_geometry_interaction.render.scatter.geometry_scatter_layers
 ES32-CTS.functional.tessellation_geometry_interaction.render.scatter.geometry_scatter_primitives
+ES2-CTS.functional.state_query.integers.viewport_getfloat = mesa ddc32537d6db69198e88ef0dfe19770bf9daa536
+ES3-CTS.functional.state_query.integers.viewport_getinteger = mesa ddc32537d6db69198e88ef0dfe19770bf9daa536
+ES3-CTS.functional.state_query.integers.viewport_getfloat = mesa ddc32537d6db69198e88ef0dfe19770bf9daa536
+ES3-CTS.functional.state_query.integers.viewport_getinteger64 = mesa ddc32537d6db69198e88ef0dfe19770bf9daa536
+ES2-CTS.functional.state_query.integers.viewport_getinteger = mesa ddc32537d6db69198e88ef0dfe19770bf9daa536
 
 [expected-crashes]
 ES31-CTS.core.texture_storage_multisample.APIGLSampleMaski
@@ -4906,11 +4911,6 @@ ESEXT-CTS.draw_elements_base_vertex_tests.AEP_shader_stages = mesa 04026b43c89c6
 ESEXT-CTS.texture_cube_map_array.texture_size_tesselation_con_sh = mesa 04026b43c89c6fdb794650f8c80e356707cc6d69
 ES31-CTS.functional.debug.negative_coverage.get_error.buffer.framebuffer_texture2d = mesa a40640f5303c9d03845459ecc364a3466d25cb5b
 ES3-CTS.functional.negative_api.buffer.framebuffer_texture2d = mesa a40640f5303c9d03845459ecc364a3466d25cb5b
-ES2-CTS.functional.state_query.integers.viewport_getfloat = cts e0c6279bf072825ff8049edbe69480f8958267a3
-ES3-CTS.functional.state_query.integers.viewport_getinteger = cts e0c6279bf072825ff8049edbe69480f8958267a3
-ES3-CTS.functional.state_query.integers.viewport_getfloat = cts e0c6279bf072825ff8049edbe69480f8958267a3
-ES3-CTS.functional.state_query.integers.viewport_getinteger64 = cts e0c6279bf072825ff8049edbe69480f8958267a3
-ES2-CTS.functional.state_query.integers.viewport_getinteger = cts e0c6279bf072825ff8049edbe69480f8958267a3
 ES31-CTS.functional.debug.negative_coverage.log.shader.program_binary = cts 846a29d056a8b41b5bebdd262c6a18d7abbae2ce
 ES31-CTS.functional.debug.negative_coverage.get_error.shader.program_binary = cts 846a29d056a8b41b5bebdd262c6a18d7abbae2ce
 ES31-CTS.functional.debug.negative_coverage.callbacks.shader.program_binary = cts 846a29d056a8b41b5bebdd262c6a18d7abbae2ce

glescts-test/bsw.conf
@@ -1,4 +1,9 @@
 [expected-failures]
+ES2-CTS.functional.state_query.integers.viewport_getfloat = mesa ddc32537d6db69198e88ef0dfe19770bf9daa536
+ES3-CTS.functional.state_query.integers.viewport_getinteger = mesa ddc32537d6db69198e88ef0dfe19770bf9daa536
+ES3-CTS.functional.state_query.integers.viewport_getfloat = mesa ddc32537d6db69198e88ef0dfe19770bf9daa536
+ES3-CTS.functional.state_query.integers.viewport_getinteger64 = mesa ddc32537d6db69198e88ef0dfe19770bf9daa536
+ES2-CTS.functional.state_query.integers.viewport_getinteger = mesa ddc32537d6db69198e88ef0dfe19770bf9daa536
 
 [expected-crashes]
 ES31-CTS.core.texture_storage_multisample.APIGLSampleMaski
@@ -495,11 +500,6 @@ ESEXT-CTS.draw_elements_base_vertex_tests.AEP_shader_stages = mesa 04026b43c89c6
 ESEXT-CTS.texture_cube_map_array.texture_size_tesselation_con_sh = mesa 04026b43c89c6fdb794650f8c80e356707cc6d69
 ES31-CTS.functional.debug.negative_coverage.get_error.buffer.framebuffer_texture2d = mesa a40640f5303c9d03845459ecc364a3466d25cb5b
 ES3-CTS.functional.negative_api.buffer.framebuffer_texture2d = mesa a40640f5303c9d03845459ecc364a3466d25cb5b
-ES2-CTS.functional.state_query.integers.viewport_getfloat = cts e0c6279bf072825ff8049edbe69480f8958267a3
-ES3-CTS.functional.state_query.integers.viewport_getinteger = cts e0c6279bf072825ff8049edbe69480f8958267a3
-ES3-CTS.functional.state_query.integers.viewport_getfloat = cts e0c6279bf072825ff8049edbe69480f8958267a3
-ES3-CTS.functional.state_query.integers.viewport_getinteger64 = cts e0c6279bf072825ff8049edbe69480f8958267a3
-ES2-CTS.functional.state_query.integers.viewport_getinteger = cts e0c6279bf072825ff8049edbe69480f8958267a3
 ES31-CTS.functional.debug.negative_coverage.log.shader.program_binary = cts 846a29d056a8b41b5bebdd262c6a18d7abbae2ce
 ES31-CTS.functional.debug.negative_coverage.get_error.shader.program_binary = cts 846a29d056a8b41b5bebdd262c6a18d7abbae2ce
 ES31-CTS.functional.debug.negative_coverage.callbacks.shader.program_binary = cts 846a29d056a8b41b5bebdd262c6a18d7abbae2ce

glescts-test/bxt.conf
@@ -1,4 +1,9 @@
 [expected-failures]
+ES2-CTS.functional.state_query.integers.viewport_getfloat = mesa ddc32537d6db69198e88ef0dfe19770bf9daa536
+ES3-CTS.functional.state_query.integers.viewport_getinteger = mesa ddc32537d6db69198e88ef0dfe19770bf9daa536
+ES3-CTS.functional.state_query.integers.viewport_getfloat = mesa ddc32537d6db69198e88ef0dfe19770bf9daa536
+ES3-CTS.functional.state_query.integers.viewport_getinteger64 = mesa ddc32537d6db69198e88ef0dfe19770bf9daa536
+ES2-CTS.functional.state_query.integers.viewport_getinteger = mesa ddc32537d6db69198e88ef0dfe19770bf9daa536
 
 [expected-crashes]
 ES32-CTS.gtf

glescts-test/hsw.conf
@@ -6407,6 +6407,11 @@ ES32-CTS.functional.texture.specification.texsubimage3d_pbo.srgb8_alpha8_cube_ar
 ES32-CTS.functional.texture.specification.texsubimage3d_pbo.srgb8_cube_array
 ESEXT-CTS.tessellation_shader.tessellation_shader_tessellation.gl_InvocationID_PatchVerticesIn_PrimitiveID = mesa 922b471777c524f497fb8eda97153a898900f223 post-12.0
 ES31-CTS.core.tessellation_shader.tessellation_shader_tessellation.gl_InvocationID_PatchVerticesIn_PrimitiveID = cts c072d3a4d0914feba493864fbc6efc172a4da455
+ES2-CTS.functional.state_query.integers.viewport_getfloat = mesa ddc32537d6db69198e88ef0dfe19770bf9daa536
+ES3-CTS.functional.state_query.integers.viewport_getinteger = mesa ddc32537d6db69198e88ef0dfe19770bf9daa536
+ES3-CTS.functional.state_query.integers.viewport_getfloat = mesa ddc32537d6db69198e88ef0dfe19770bf9daa536
+ES3-CTS.functional.state_query.integers.viewport_getinteger64 = mesa ddc32537d6db69198e88ef0dfe19770bf9daa536
+ES2-CTS.functional.state_query.integers.viewport_getinteger = mesa ddc32537d6db69198e88ef0dfe19770bf9daa536
 
 [expected-crashes]
 ES31-CTS.core.texture_storage_multisample.APIGLSampleMaski

glescts-test/kbl.conf
@@ -1,4 +1,9 @@
 [expected-failures]
+ES2-CTS.functional.state_query.integers.viewport_getfloat = mesa ddc32537d6db69198e88ef0dfe19770bf9daa536
+ES3-CTS.functional.state_query.integers.viewport_getinteger = mesa ddc32537d6db69198e88ef0dfe19770bf9daa536
+ES3-CTS.functional.state_query.integers.viewport_getfloat = mesa ddc32537d6db69198e88ef0dfe19770bf9daa536
+ES3-CTS.functional.state_query.integers.viewport_getinteger64 = mesa ddc32537d6db69198e88ef0dfe19770bf9daa536
+ES2-CTS.functional.state_query.integers.viewport_getinteger = mesa ddc32537d6db69198e88ef0dfe19770bf9daa536
 
 [expected-crashes]
 ES31-CTS.core.texture_storage_multisample.APIGLSampleMaski
@@ -511,11 +516,6 @@ ES2-CTS.gtf.GL2ExtensionTests.egl_image_external.TestVertexShader = mesa af8ad49
 ES31-CTS.functional.debug.negative_coverage.get_error.buffer.framebuffer_texture2d = mesa a40640f5303c9d03845459ecc364a3466d25cb5b
 ES32-CTS.functional.debug.negative_coverage.get_error.buffer.framebuffer_texture2d = mesa a40640f5303c9d03845459ecc364a3466d25cb5b
 ES3-CTS.functional.negative_api.buffer.framebuffer_texture2d = mesa a40640f5303c9d03845459ecc364a3466d25cb5b
-ES2-CTS.functional.state_query.integers.viewport_getfloat = cts e0c6279bf072825ff8049edbe69480f8958267a3
-ES3-CTS.functional.state_query.integers.viewport_getinteger = cts e0c6279bf072825ff8049edbe69480f8958267a3
-ES3-CTS.functional.state_query.integers.viewport_getfloat = cts e0c6279bf072825ff8049edbe69480f8958267a3
-ES3-CTS.functional.state_query.integers.viewport_getinteger64 = cts e0c6279bf072825ff8049edbe69480f8958267a3
-ES2-CTS.functional.state_query.integers.viewport_getinteger = cts e0c6279bf072825ff8049edbe69480f8958267a3
 ES32-CTS.functional.debug.negative_coverage.callbacks.shader.program_binary = cts 846a29d056a8b41b5bebdd262c6a18d7abbae2ce
 ES32-CTS.functional.debug.negative_coverage.log.shader.program_binary = cts 846a29d056a8b41b5bebdd262c6a18d7abbae2ce
 ES31-CTS.functional.debug.negative_coverage.log.shader.program_binary = cts 846a29d056a8b41b5bebdd262c6a18d7abbae2ce

glescts-test/skl.conf
@@ -1,4 +1,9 @@
 [expected-failures]
+ES2-CTS.functional.state_query.integers.viewport_getfloat = mesa ddc32537d6db69198e88ef0dfe19770bf9daa536
+ES3-CTS.functional.state_query.integers.viewport_getinteger = mesa ddc32537d6db69198e88ef0dfe19770bf9daa536
+ES3-CTS.functional.state_query.integers.viewport_getfloat = mesa ddc32537d6db69198e88ef0dfe19770bf9daa536
+ES3-CTS.functional.state_query.integers.viewport_getinteger64 = mesa ddc32537d6db69198e88ef0dfe19770bf9daa536
+ES2-CTS.functional.state_query.integers.viewport_getinteger = mesa ddc32537d6db69198e88ef0dfe19770bf9daa536
 
 [expected-crashes]
 ES32-CTS.gtf
@@ -535,11 +540,6 @@ ES2-CTS.gtf.GL2ExtensionTests.egl_image_external.TestVertexShader = mesa af8ad49
 ES31-CTS.functional.debug.negative_coverage.get_error.buffer.framebuffer_texture2d = mesa a40640f5303c9d03845459ecc364a3466d25cb5b
 ES32-CTS.functional.debug.negative_coverage.get_error.buffer.framebuffer_texture2d = mesa a40640f5303c9d03845459ecc364a3466d25cb5b
 ES3-CTS.functional.negative_api.buffer.framebuffer_texture2d = mesa a40640f5303c9d03845459ecc364a3466d25cb5b
-ES2-CTS.functional.state_query.integers.viewport_getfloat = cts e0c6279bf072825ff8049edbe69480f8958267a3
-ES3-CTS.functional.state_query.integers.viewport_getinteger = cts e0c6279bf072825ff8049edbe69480f8958267a3
-ES3-CTS.functional.state_query.integers.viewport_getfloat = cts e0c6279bf072825ff8049edbe69480f8958267a3
-ES3-CTS.functional.state_query.integers.viewport_getinteger64 = cts e0c6279bf072825ff8049edbe69480f8958267a3
-ES2-CTS.functional.state_query.integers.viewport_getinteger = cts e0c6279bf072825ff8049edbe69480f8958267a3
 ES32-CTS.functional.debug.negative_coverage.callbacks.shader.program_binary = cts 846a29d056a8b41b5bebdd262c6a18d7abbae2ce
 ES32-CTS.functional.debug.negative_coverage.log.shader.program_binary = cts 846a29d056a8b41b5bebdd262c6a18d7abbae2ce
 ES31-CTS.functional.debug.negative_coverage.log.shader.program_binary = cts 846a29d056a8b41b5bebdd262c6a18d7abbae2ce