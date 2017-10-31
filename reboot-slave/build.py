c1e9e5902a937372562e7a1ef36f45f9aa52c3d4
tokonbekov <mirlan.ax.tokonbekov@intel.com>
2017-10-31 16:36:34 -0700

    Convert nightly reboot away from hand-maintained xml

--- reboot-slave/build.py
+++ reboot-slave/build.py
@@ -3,15 +3,24 @@
 import os
 import sys
 import urllib2
+import ast
+import time
 sys.path.append(os.path.join(os.path.dirname(os.path.abspath(sys.argv[0])), ".."))
 import build_support as bs
-
-# Leeroy runs the builds inside of a user namespace, so it can bind
-# mount on /tmp.  This prevents the use of sudo.  This build just
-# triggers the "single_reboot" job, which does not run in a user
-# namespace.
-
 server = bs.ProjectMap().build_spec().find("build_master").attrib["host"]
-url = "http://" + server + "/job/reboot_single/buildWithParameters?token=noauth&label=" + bs.Options().hardware
-print "opening: " + url
-urllib2.urlopen(url)
+
+url = "http://" + server + "/computer/api/python"
+f=urllib2.urlopen(url)
+host_dict = ast.literal_eval(f.read())
+
+def is_excluded():
+    if ("builder" in host or host == "master"):
+        return True
+
+for a_host in host_dict['computer']:
+    host = a_host['displayName']
+    if is_excluded():
+        continue
+    url = "http://" + server + "/job/reboot_single/buildWithParameters?token=noauth&label=" + host
+    urllib2.urlopen(url)
+    time.sleep(10)
