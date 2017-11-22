f2affdbe5522b07ce8a1c2eb08c78319a5998ea1
Dylan Baker <dylan@pnwbakers.com>
2017-11-22 15:56:51 -0800

    meson-buildtest: run most unit tests
    There are a couple of unit tests that are not run here, the symbol check
    tests.
    
    mesonbuild/meson#2675

--- meson-buildtest/build.py
+++ meson-buildtest/build.py
@@ -25,6 +25,67 @@ def main():
         options.extend(['-Dbuildtype=release', '-Db_ndebug=true'])
     b = bs.builders.MesonBuilder(extra_definitions=options, install=False)
 
+    b.tests += [
+        # TODO: These need runtime discovery, probably using `find` or to point
+        # at the DSOs in the install directory
+        #
+        #'es1-ABI-check',
+        #'es2-ABI-check',
+        #'gbm-symbols-check',
+        #'wayland-egl-symbols-check',
+        #'wayland-egl-abi-check',
+        #'egl-symbols-check',
+        #'egl-entrypoint-check',
+
+        'anv_block_pool_no_free',
+        'anv_state_pool',
+        'anv_state_pool_free_list_only',
+        'anv_state_pool_no_free',
+        'blob_test',
+        'cache_test',
+        'clear',
+        'collision',
+        'delete_and_lookup',
+        'delete_management',
+        'destroy_callback',
+        'eu_compact',
+        'glx-dispatch-index-check',
+        'insert_and_lookup',
+        'insert_many',
+        'isl_surf_get_image_offset',
+        'lp_test_arit',
+        'lp_test_blend',
+        'lp_test_conv',
+        'lp_test_format',
+        'lp_test_printf',
+        'mesa-sha1',
+        'null_destroy',
+        'random_entry',
+        'remove_null',
+        'replacement',
+        'roundeven',
+        'u_atomic',
+    ]
+
+    b.gtests += [
+        'eu_validate',
+        'fs_cmod_propagation',
+        'fs_copy_propagation',
+        'fs_saturate_propagation',
+        'general_ir_test',
+        'glx-test',
+        'main-test',
+        'nir_control_flow',
+        'sampler_types_test',
+        'shared-glapi-test',
+        'string_buffer',
+        'uniform_initializer_test',
+        'vec4_cmod_propagation',
+        'vec4_copy_propagation',
+        'vec4_register_coalesce',
+        'vf_float_conversions',
+    ]
+
     try:
         bs.build(b)
     except subprocess.CalledProcessError as e:
